8c321e08147fa920c192618f123f13fe45b58143
Jaewon Chung <contact@iamjae.one>
2026-03-31 17:18:49 +0900

    feat: edited

--- Static_Code/Reference_Code/ASW/SWC_Switch.c
+++ Static_Code/Reference_Code/ASW/SWC_Switch.c
@@ -5,9 +5,9 @@ void RE_Switch()
     IoHwAb_LevelType lv;
     Rte_Call_R_Switch1_ReadDirect(lv);
     uint32 magic = 0xBA5EBA11;
+    
     while(magic != 0x0)
     {
         Rte_Call_R_TestInterface_TestOperation((lv == IOHWAB_HIGH), &magic);
     }
-
 }
