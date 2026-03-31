fcf3f140dd328ce684baaecaf3f3c49461ff3f06
Jaewon Chung <contact@iamjae.one>
2026-03-31 16:38:36 +0900

    feat: Add Switch SW Component

---
+++ Static_Code/Reference_Code/ASW/SWC_Switch.c
@@ -0,0 +1,12 @@
+#include "Rte_SWC_Switch.h"
+
+void RE_Switch()
+{
+    IoHwAb_LevelType lv;
+    Rte_Call_R_Switch1_ReadDirect(lv);
+    uint32 magic = 0xBA5EBA11;
+    while(magic != 0x0)
+    {
+        Rte_Call_R_TestInterface_TestOperation((lv == IOHWAB_HIGH), &magic);
+    }
+}
