446a7440814540ecfdf6d50ccc8e48ab446d1ac6
Jaewon Chung <contact@iamjae.one>
2026-04-01 20:35:09 +0900

    fix

--- Static_Code/Reference_Code/ASW/SWC_Switch.c
+++ Static_Code/Reference_Code/ASW/SWC_Switch.c
@@ -1,12 +1,10 @@
 // cppcheck-suppress missingInclude
 #include "Rte_SWC_Switch.h"
 
-void RE_Switch(void);
-
 // cppcheck-suppress unusedFunction
+// cppcheck-suppress misra-c2012-8.7
 void RE_Switch(void)
 {
-    // hello world bye
     IoHwAb_LevelType lv;
     Rte_Call_R_Switch1_ReadDirect(lv);
     uint32 magic = 0xBA5EBA11U;
